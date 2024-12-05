b5814911c9d1d095b555082c4e54b6e24e5265e2
碧蓝小新星 <2745672197@qq.com>
2024-12-05 15:32:02 +0800

    11.29 java  第12次实验 检查备份
    购物车模块基本实现。 有乱码问题。

--- webTest/src/main/java/com/itheima/Servlet/cart/common/AppConfigConstants.java
+++ webTest/src/main/java/com/itheima/Servlet/cart/common/AppConfigConstants.java
@@ -4,10 +4,7 @@ public class AppConfigConstants {
     public static String CART_KEY = "cart";
     public static String LOGGED_IN_SESSION_ATTR = "loggedIn";
     public static String LOGGED_SUCCESS_VALUE = "success";
-    public static String ITEM_ID_KEY = "itemId";
-    public static String COVER_IMG_KEY = "coverImg";
     public static String ERROR_MESSAGE_KEY = "errorMessage";
-    public static String SESSION_ID_KEY = "JSESSIONID";
     public static String CART_JSP_URL = "/cart.jsp";
     public static String ERROR_JSP_URL = "/error.jsp";
     public static String ITEM_JSP_URL = "/item.jsp";
