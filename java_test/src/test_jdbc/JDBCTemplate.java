2d96e8054853c6e050ef2cbbe7e6962dc17cf7b6
碧蓝小新星 <2745672197@qq.com>
2025-03-10 10:16:41 +0800

    3.10 jdbc 链接实现

---
+++ java_test/src/test_jdbc/JDBCTemplate.java
@@ -0,0 +1,52 @@
+package test_jdbc;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class JDBCTemplate {
+    private Connection connection = null;
+
+
+    public JDBCTemplate() {
+        connection = null;
+        try {
+            connection = JDBCUtils.getConnection();
+            System.out.println("JDBC连接建立完成");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("建立连接失败");
+        }
+    }
+
+    public <T> List querForObject(String sql, Class<T> cla, List<Object> args) throws SQLException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
+        PreparedStatement statement = connection.prepareStatement(sql);
+        List<T> results = new ArrayList<T>();
+        for (int i = 0; i < args.size(); i++) {
+            statement.setObject(1 + i, args.get(i));
+        }
+        ResultSet resultSet = statement.executeQuery();
+        while (resultSet.next()) {
+            T result = cla.getDeclaredConstructor().newInstance();
+            int columnCount = resultSet.getMetaData().getColumnCount();
+            Field[] fields = cla.getDeclaredFields();
+            Map<String, Field> map = new HashMap<>();
+            for (Field field : fields) {
+                field.setAccessible(true);
+                map.put(field.getName(), field);
+            }
+            for (int i = 0; i < columnCount; i++) {
+                String columnName = resultSet.getMetaData().getColumnName(i + 1);
+                if (map.containsKey(columnName)) {
+                    map.get(columnName).set(result, resultSet.getObject(i + 1));
+                }
+            }
+            results.add(result);
+        }
+        return results;
+    }
+}
---
+++ java_test/src/test_proxy/IStudent.java
@@ -0,0 +1,7 @@
+package test_proxy;
+
+import javax.swing.*;
+
+public interface IStudent {
+    public void doExercise(String courseName);
+}
